92e360bcb494459f738a54ff28b5ae36c8c24acb
Ali Ahmed <aliahmedaiub@gmail.com>
2016-08-04 03:11:58 +0600

    from home

--- ContactListView/app/src/main/java/com/example/bs148/contactlistview/MainActivity.java
+++ ContactListView/app/src/main/java/com/example/bs148/contactlistview/MainActivity.java
@@ -9,6 +9,7 @@ import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,6 +17,7 @@ import android.widget.ImageView;
 import com.example.bs148.contactlistview.databinding.ActivityMainBinding;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -68,15 +70,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        img_photo.setImageURI(photoURI);
+       // img_photo.setImageURI(photoURI);
         try{
             Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),photoURI);
-            createDirectoryAndSaveFile(bitmap,(""+ (++image_number)));
+            img_photo.setImageBitmap(bitmap);
+          //  createDirectoryAndSaveFile(bitmap,(""+ (++image_number)));
         }catch (Exception e){
 
         }
 
-        String directory_name = Environment.getExternalStorageDirectory().getPath()+"/Prisma/" + (++image_number) +".jpg";
+       // String directory_name = Environment.getExternalStorageDirectory().getPath()+"/Prisma/" + (++image_number) +".jpg";
     }
 
     private File createImageFile() throws IOException {
@@ -92,23 +95,4 @@ public class MainActivity extends AppCompatActivity {
         return image;
     }
 
-    private void createDirectoryAndSaveFile(Bitmap imageToSave, String fileName) {
-        File direct = new File(Environment.getExternalStorageDirectory() + "/MyCustom");
-        if (!direct.exists()) {
-            File wallpaperDirectory = new File("/sdcard/MyCustom/");
-            wallpaperDirectory.mkdirs();
-        }
-        File file = new File(new File("/sdcard/MyCustom/"), fileName);
-        if (file.exists()) {
-            file.delete();
-        }
-        try {
-            FileOutputStream out = new FileOutputStream(file);
-            imageToSave.compress(Bitmap.CompressFormat.JPEG, 100, out);
-            out.flush();
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
